12af3b48a4e4bd5cc5bf1716424d0bf32f282fad
Heechul So <thrrkthrrk@gmail.com>
2021-01-18 14:02:16 +0900

    Add h2 database file, getUsers

--- src/Application.kt
+++ src/Application.kt
@@ -2,18 +2,22 @@ package com.example
 
 import com.example.lotto.LottoGenerator
 import com.example.user.Users
+import com.example.web.PostUser
 import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.freemarker.*
 import io.ktor.jackson.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.h2.tools.Server
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
@@ -25,7 +29,15 @@ fun Application.module() {
         jackson()
     }
 
-    Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092", "-key", "test", "~/test").start()
+    Server.createTcpServer(
+        "-tcp",
+        "-tcpAllowOthers",
+        "-tcpPort",
+        "9092",
+        "-key",
+        "test",
+        File(".").absolutePath + "/h2/test"
+    ).start()
     Database.connect(url = "jdbc:h2:tcp://localhost:9092/test", driver = "org.h2.Driver")
 
     transaction {
@@ -43,19 +55,34 @@ fun Application.module() {
             )
         }
 
-        post("/user") {
+        post("/users") {
+            val content = call.receive<PostUser>()
             var id: Int? = null
             transaction {
                 id = Users.insert {
-                    it[name] = "name"
-                    it[gender] = "M"
-                    it[phone] = "01012341234"
+                    it[name] = content.name
+                    it[gender] = content.gender.toString()
+                    it[phone] = content.phone
                 }[Users.id].value
             }
 
             call.respond(object {
                 val userId = id
             })
+        }
+
+        get("/users") {
+            var temp: Any? = null
+            transaction {
+                temp = Users.selectAll().map {
+                    object {
+                        val name: String = it[Users.name]
+                        val gender: String = it[Users.gender]
+                        val phone: String = it[Users.phone]
+                    }
+                }
+            }
+            call.respond(temp as Any)
         }
 
         get("/") {
---
+++ src/web/Requests.kt
@@ -0,0 +1,3 @@
+package com.example.web
+
+data class PostUser(val name: String, val gender: Char, val phone: String)
